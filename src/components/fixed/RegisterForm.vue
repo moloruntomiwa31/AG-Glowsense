6a90f19f75be6dfd068b278817ac21316d2ad002
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2024-10-16 15:07:18 +0100

    #form cleanup

--- src/components/fixed/RegisterForm.vue
+++ src/components/fixed/RegisterForm.vue
@@ -1,5 +1,11 @@
 <template>
-  <form class="w-[80%] md:w-[50%]" @submit.prevent="submitForm">
+  <form class="w-[80%] md:w-[65%] lg:w-[50%]" @submit.prevent="submitForm">
+    <Button
+      @click="loginWithGoogle"
+      class="bg-gray-900 p-3 gap-4 flex items-center justify-center text-white hover:bg-gray-700 duration-150 w-full mt-4"
+      ><img src="../../assets/google.svg" alt="Google" />Signup with
+      Google</Button
+    >
     <div class="my-3 grid grid-cols-2 gap-4">
       <div class="col-span-2 md:col-span-1">
         <label for="firstName" class="text-2xl text-white">First Name</label>
@@ -23,20 +29,17 @@
           placeholder="enter your last name"
         />
       </div>
-      <div class="col-span-2">
+      <div class="col-span-2 md:col-span-1">
         <label for="phoneNumber" class="text-2xl text-white"
           >Phone Number</label
         >
-        <input
-          id="phoneNumber"
-          type="tel"
-          required
+        <vue-tel-input
+          class="p-2 !border-red-400 !border"
           v-model="phoneNumber"
-          class="rounded-md text-xl shadow-sm p-3 outline-red-400 shadow-red-400 w-full"
-          placeholder="123-456-7890"
-        />
+          mode="international"
+        ></vue-tel-input>
       </div>
-      <div class="col-span-2">
+      <div class="col-span-2 md:col-span-1">
         <label for="email" class="text-2xl text-white">Email Address</label>
         <input
           id="email"
@@ -78,33 +81,6 @@
       class="bg-red-400 p-3 text-white hover:bg-red-500 duration-150 w-full mt-4"
       >{{ buttonText }}</Button
     >
-    <Button
-      @click="loginWithGoogle"
-      class="bg-gray-900 p-3 gap-4 flex items-center justify-center text-white hover:bg-gray-700 duration-150 w-full mt-4"
-      ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 48 48"
-      >
-        <path
-          fill="#ffc107"
-          d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917"
-        />
-        <path
-          fill="#ff3d00"
-          d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691"
-        />
-        <path
-          fill="#4caf50"
-          d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.9 11.9 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44"
-        />
-        <path
-          fill="#1976d2"
-          d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917"
-        /></svg
-      >Signup with Google</Button
-    >
   </form>
 </template>
 
@@ -112,6 +88,8 @@
 import { ref } from "vue";
 import Button from "./Button.vue";
 import { useGoogle } from "../../composables/useGoogle";
+import { VueTelInput } from "vue-tel-input";
+import "vue-tel-input/vue-tel-input.css";
 
 const emailInput = ref("");
 const passwordInput = ref("");
@@ -144,3 +122,9 @@ const toggleVisibility = () => {
   showPassword.value = !showPassword.value;
 };
 </script>
+
+<style scoped>
+.vti__dropdown {
+  background-color: aliceblue;
+}
+</style>
--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="flex flex-col justify-center items-center min-h-[100vh] logIn">
     <h1 class="text-4xl font-bold text-white">Log in</h1>
-    <form class="w-[80%] md:w-[50%]" @submit.prevent="submitForm">
+    <form class="w-[80%] md:w-[65%] lg:w-[50%]" @submit.prevent="submitForm">
+      <Button
+        @click="loginWithGoogle"
+        class="bg-gray-900 p-3 gap-4 flex items-center justify-center text-white hover:bg-gray-700 duration-150 w-full mt-4"
+        ><img src="../assets/google.svg" alt="google" />Continue with
+        Google</Button
+      >
       <div class="my-3 grid grid-cols-2 gap-4">
         <div class="col-span-2">
           <label for="email" class="text-2xl text-white">Email Address</label>
@@ -45,33 +51,6 @@
         type="submit"
         class="bg-red-400 p-3 text-white hover:bg-red-500 duration-150 w-full mt-4"
         >{{ buttonText }}</Button
-      >
-      <Button
-        @click="loginWithGoogle"
-        class="bg-gray-900 p-3 gap-4 flex items-center justify-center text-white hover:bg-gray-700 duration-150 w-full mt-4"
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1em"
-          height="1em"
-          viewBox="0 0 48 48"
-        >
-          <path
-            fill="#ffc107"
-            d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917"
-          />
-          <path
-            fill="#ff3d00"
-            d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691"
-          />
-          <path
-            fill="#4caf50"
-            d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.9 11.9 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44"
-          />
-          <path
-            fill="#1976d2"
-            d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917"
-          /></svg
-        >Login with Google</Button
       >
     </form>
 
